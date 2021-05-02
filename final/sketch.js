0200e30e80a605fecf2deecab8dd57c19e52e8c6
Omar Nema <omar.nema@arcadiasolutions.com>
2021-05-02 13:35:01 -0400

    boyy

--- final/sketch.js
+++ final/sketch.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   
+   
+    document.querySelector('.btn.info').addEventListener('click', (e)=> {
+        let infoOverlay = document.querySelector('.info-overlay');
+        infoOverlay.classList.toggle('show');
+        if (infoOverlay.classList.contains('show')){
+            e.target.innerHTML = 'X';
+        } else {
+            e.target.innerHTML = '?';      
+        }
+    })
 
 });
